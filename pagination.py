295a06896abd2faaa191f5135bc6e16def4239bc
mirian pacheco <mirian.alvesp@gmail.com>
2020-06-18 00:30:37 +0200

    pagination algorithm

--- pagination.py
+++ pagination.py
@@ -1,19 +1,19 @@
 def pagination(current_page, total_pages, boundaries, around):
     page_range = []
     page_range_with_dots = []
-    l = None
+    verify_page = None
 
     for page in range(1, total_pages + 1):
         if (page <= boundaries and page < current_page) or page >= (total_pages - boundaries + 1) or page >= (current_page - around) and page <= (current_page + around):
             page_range.append(page)
     
     for page in page_range:
-        if l is not None:
-            if page - l != 1:
+        if verify_page is not None:
+            if page - verify_page != 1:
                 page_range_with_dots.append('...')
                 
         page_range_with_dots.append(page)
-        l = page
+        verify_page = page
 
     print(page_range_with_dots)
     return page_range_with_dots
