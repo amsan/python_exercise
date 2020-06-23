14b1baf3f0161bbf28d99458eb01ea45b3ae794e
mirian pacheco <mirian.alvesp@gmail.com>
2020-06-23 15:26:54 +0200

    refactor code and add tests

--- pagination.py
+++ pagination.py
@@ -1,18 +1,15 @@
 def pagination(current_page, total_pages, boundaries, around):
-    page_range = []
+    pages = range(1, total_pages + 1)
+    page_range = list(filter(lambda page: (page <= boundaries and page < current_page) or page >= (total_pages - boundaries + 1) or page >= (current_page - around) and page <= (current_page + around), pages))
     page_range_with_dots = []
+    append = page_range_with_dots.append
     verify_page = None
 
-    for page in range(1, total_pages + 1):
-        if (page <= boundaries and page < current_page) or page >= (total_pages - boundaries + 1) or page >= (current_page - around) and page <= (current_page + around):
-            page_range.append(page)
-    
     for page in page_range:
-        if verify_page is not None:
-            if page - verify_page != 1:
-                page_range_with_dots.append('...')
+        if verify_page is not None and page - verify_page != 1:
+            append('...')
                 
-        page_range_with_dots.append(page)
+        append(page)
         verify_page = page
 
     print(page_range_with_dots)
--- test.py
+++ test.py
@@ -5,14 +5,27 @@ import pagination
 class TestPagination(unittest.TestCase): 
     def test_pagination(self):
         """
-        Should give no page around 4, 1 page in the beginning and 1 page in the end
+        Should return no page around 4, 1 page in the beginning and 1 page in the end
         """
         self.assertEqual(pagination.pagination(4, 5, 1, 0), [1, '...', 4, 5])
     
     def test_pagination_boundaries(self):
         """
-        Should give 2 pages around 4, 2 pages in the beginning and 2 pages in the end
+        Should return 2 pages around 4, 2 pages in the beginning and 2 pages in the end
         """
         self.assertEqual(pagination.pagination(4, 10, 2, 2), [1, 2, 3, 4, 5, 6, '...', 9, 10])
+
+    def test_pagination_invalid(self):
+        """
+        Should return number of pages when total pages number is smaller than current page 
+        """
+        self.assertEqual(pagination.pagination(4, 2, 2, 2), [1, 2])
+
+    def test_pagination_invalid_boundaries(self):
+        """
+        Should return number of pages when total pages number is smaller than boundaries 
+        """
+        self.assertEqual(pagination.pagination(4, 3, 5, 2), [1, 2, 3])
+
 if __name__ == '__main__': 
     unittest.main() 
