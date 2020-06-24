a16d6db7ad1b2b05dc8349a0e43b1ece667afaa3
António Maltsev Santos <antonio.maltsev@gmail.com>
2020-06-24 14:50:02 +0100

    add test_pagination_performance test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 
 import pagination
 
@@ -15,6 +16,31 @@ class TestPagination(unittest.TestCase):
         """
         self.assertEqual(pagination.pagination(4, 10, 2, 2), [1, 2, 3, 4, 5, 6, '...', 9, 10])
 
+    def test_pagination_performance(self):
+        """
+        Should return quickly, even when total pages number is very large
+        """
+        
+        before = time.time()
+        
+        self.assertEqual(
+            pagination.pagination(
+                current_page=10000, 
+                total_pages=50000000,
+                boundaries=2,
+                around=2
+             ),
+            [1, 2, "...", 9998, 9999, 10000, 10001, 10002, "...", 49999999, 50000000],
+        )
+        
+        after = time.time()
+        elapsed_seconds = after - before
+        
+        self.assertTrue(
+            elapsed_seconds < 1,
+            f"pagination took too long ({elapsed_seconds} seconds)"
+        )
+        
     def test_pagination_invalid(self):
         """
         Should return number of pages when total pages number is smaller than current page 
